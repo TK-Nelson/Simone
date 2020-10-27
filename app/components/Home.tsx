73c00ca6bca854d7bbb21cecad092c6874c6f038
Tiko <blacksheep493@gmail.com>
2020-10-26 22:36:01 -0400

    Set up basic layout with sass and added button to test base functionalty

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
-import styles from './Home.css';
+import styles from './Home.scss';
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+    <div className={styles.appWrapper}>
+      <div className={styles.leftNav} data-tid="leftNav">
+        <p>test</p>
+      </div>
+
+      <div className={styles.rightContent} data-tid="container">
+      <div>
+        <h2>Home</h2>
+        <Link to={routes.COUNTER}>to Counter</Link>
+      </div>
+
+        <button onClick="" >Test</button>
+      </div>
+
     </div>
   );
 }
